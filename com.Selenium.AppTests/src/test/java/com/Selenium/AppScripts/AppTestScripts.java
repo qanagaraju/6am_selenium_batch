8896912a6f6a0e0849eae5f97e7b425f4f2ad36f
nagarajuqatrainer <63718824+nagarajuqatrainer@users.noreply.github.com>
2021-02-02 06:59:32 +0530

    SecondCommit

--- com.Selenium.AppTests/src/test/java/com/Selenium/AppScripts/AppTestScripts.java
+++ com.Selenium.AppTests/src/test/java/com/Selenium/AppScripts/AppTestScripts.java
@@ -1,15 +1,38 @@
 package com.Selenium.AppScripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class AppTestScripts 
 {
-    
+    public static WebDriver driver;
+	public static String expurl = "https://www.mycontactform.com/";
+	
+	
     @Test
-    public void launchbrowser()
+    public void launchbrowser() throws Exception
     {
        
-    	System.out.println("Java Selenium Test");
+    	System.setProperty("webdriver.chrome.driver", "./myDrivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		
+		driver.get(expurl);
+		
+		Thread.sleep(3000);
+		
+		if(expurl.equals(driver.getCurrentUrl())) {
+			System.out.println("Verified url");
+		}
+		else
+		{
+			System.out.println("Does not verified");
+		}
+		
+		
+		
+		driver.close();
+    	
     	
     	
     }
