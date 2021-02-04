0a731fee6ef0aa640b1db98aea298ef05ba34f95
nagarajuqatrainer <63718824+nagarajuqatrainer@users.noreply.github.com>
2021-02-04 06:15:18 +0530

    FinalCommit

--- com.Selenium.AppTests/src/main/java/com/AppObjects/AppLogin.java
+++ com.Selenium.AppTests/src/main/java/com/AppObjects/AppLogin.java
@@ -1,12 +1,18 @@
 package com.AppObjects;
 
+import org.openqa.selenium.WebDriver;
 
+import org.testng.annotations.Test;
 
 public class AppLogin {
 	
+	public static WebDriver driver;
+	
+	@Test
 	public class AppTestScripts 
 	{
-	    
+	   
+		
 	   
 	}
 
--- com.Selenium.AppTests/src/test/java/com/Selenium/AppUtilties/MavenSampleTest.java
+++ com.Selenium.AppTests/src/test/java/com/Selenium/AppUtilties/MavenSampleTest.java
@@ -1,10 +1,17 @@
 package com.Selenium.AppUtilties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MavenSampleTest {
+	
+	 public static WebDriver driver;
+		public static String expurl = "https://www.mycontactform.com/";
+	
+	
   @Test
-  public void f() {
+  public void f() throws Exception {
 	  
 	  //How to push maven project to GitHub server
 	  //1) Open git.com
@@ -18,8 +25,25 @@ public class MavenSampleTest {
 	  //2) Select Team >Share Project
 	  
 	  
-	  
+	System.out.println("Sample Test");  
+	System.setProperty("webdriver.chrome.driver", "./myDrivers/chromedriver.exe");
+	driver=new ChromeDriver();
 	
+	driver.get(expurl);
+	
+	Thread.sleep(3000);
+	
+	if(expurl.equals(driver.getCurrentUrl())) {
+		System.out.println("Verified url");
+	}
+	else
+	{
+		System.out.println("Does not verified");
+	}
+	
+	
+	
+	driver.close();
 	  
   }
 }
